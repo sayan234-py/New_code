6339f285fefa0be8253053a17ef0a51a1e847d07
Sayan Nandi <nsayan520@gmail.com>
2026-09-18 01:01:51 +0530

    Delete pr1.cpp

--- pr1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-    std::cout << "Write a program that prints your name and college name on two separate lines." << std::endl;
-    std::cout << "My name is Sayan Nandi" << std::endl;
-    std::cout << "My college name is Dr. B. C. Roy Academy of professional courses." << std::endl;
-    return 0;
-}
